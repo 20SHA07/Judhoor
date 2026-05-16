4019abe7619545c2a62a52ce5c2d70bd8bfe4300
Shahad Mohamed Rikas <170943064+20SHA07@users.noreply.github.com>
2026-05-16 18:04:44 +0400

    Polish responsive UI and Pages fallback

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import { HashRouter } from "react-router-dom";
 import JudhoorApp from "./JudhoorApp";
 import "./judhoor.css";
+import "./polish.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
